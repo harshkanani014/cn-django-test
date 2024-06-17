fa4d21a7e1f13a2112ef080bb86590b1a04dfa4e
harshp014 <harsh.kanani@apprendere.com>
2024-06-17 16:24:02 +0530

    ram 10000

--- first_app/views.py
+++ first_app/views.py
@@ -7,8 +7,8 @@ from decouple import config
 def index(request):
     # test_task.delay()
     large_list = []
-    for i in range(1000): 
-        large_list.append("hello world" * 1000)  # Create a large string and append to the list
+    for i in range(10000): 
+        large_list.append("hello world" * 10000)  # Create a large string and append to the list
 
     total_length = sum(len(s) for s in large_list)
 
