d7801c914d9b1097574d4bc8751e281987ec28a1
harshp014 <harsh.kanani@apprendere.com>
2024-06-17 16:36:40 +0530

    ram 10000

--- first_app/views.py
+++ first_app/views.py
@@ -8,7 +8,7 @@ def index(request):
     # test_task.delay()
     large_list = []
     for i in range(10000): 
-        large_list.append("hello world" * 10000)  # Create a large string and append to the list
+        large_list.append("hello world" * 1000)  # Create a large string and append to the list
 
     total_length = sum(len(s) for s in large_list)
 
