7091011956fd6a0d6b2a16e9fc69134fff066419
harshp014 <harsh.kanani@apprendere.com>
2024-06-17 20:01:13 +0530

    update

--- first_app/views.py
+++ first_app/views.py
@@ -12,4 +12,4 @@ def index(request):
 
     total_length = sum(len(s) for s in large_list)
 
-    return HttpResponse(f'HHM.....Hello World! This is CN Django Test.By - Harsh Kanani aa.....web hooks test harsh kanani webhook testing 123478 - harsh127890 {total_length}')
+    return HttpResponse(f'HHM.....Hello World! This is CN Django Test.By - Harsh Kanani aa.....web hooks test harsh kanani webhook testing 123478 - harsh {total_length}')
