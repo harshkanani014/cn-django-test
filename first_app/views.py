cff0cbfccf6ce25f63abbc346d66b849f5d9c370
harshp014 <harsh.kanani@apprendere.com>
2024-06-24 13:45:10 +0530

    add debugger

--- first_app/views.py
+++ first_app/views.py
@@ -11,5 +11,6 @@ def index(request):
         large_list.append("hello world" * 10)  # Create a large string and append to the list
 
     total_length = sum(len(s) for s in large_list)
+    print(large_list)
 
     return HttpResponse(f'HHM.....Hello World! This is CN Django Test.By - Harsh Kanani aa.....web hooks test harsh kanani webhook testing 123478 - harsh {total_length}')
